7c7c90963f54461f471625ddf31c5d42b86b250f
02LeeHeeSu <lee46911375@gmail.com>
2022-09-21 17:29:10 +0900

    원 운동 추가.

--- Folder04/move_children.py
+++ Folder04/move_children.py
@@ -44,3 +44,14 @@ def move_square():
         character.draw_now(x, y)
         x += 2
         delay(0.001)
+
+def move_circle():
+    angle = 0
+
+    while (angle < 360):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(400 + 210 * math.sin(angle / 360 * 2 * math.pi), 300 - 210 * math.cos(angle / 360 * 2 * math.pi))
+        angle += 1
+        delay(0.001)
+
