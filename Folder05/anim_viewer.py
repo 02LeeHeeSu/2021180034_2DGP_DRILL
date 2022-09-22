a4bfaafc662164eb72f65065b2a5ab0f3e1c2efc
02LeeHeeSu <lee46911375@gmail.com>
2022-09-22 14:05:13 +0900

    각 프레임 별로 함수를 정의하고 확인해봄.

--- Folder05/anim_viewer.py
+++ Folder05/anim_viewer.py
@@ -4,4 +4,35 @@ open_canvas()
 
 character = load_image('The_Legend_of_Zelda_Link_Sprite_Sheet.png')
 
+
+def face_frame():
+    print('Face')
+
+
+def front_frame():
+    print('Front')
+
+
+def left_frame():
+    print('Left')
+
+
+def back_frame():
+    print('Back')
+
+
+def right_frame():
+    print('Right')
+
+
+face_frame()
+
+front_frame()
+
+left_frame()
+
+back_frame()
+
+right_frame()
+
 close_canvas()
