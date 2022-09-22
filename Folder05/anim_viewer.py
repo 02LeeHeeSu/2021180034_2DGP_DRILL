4d4537db6c4414f745494aab026686df73a45f86
02LeeHeeSu <lee46911375@gmail.com>
2022-09-22 14:36:10 +0900

    face_frame에서 얼굴이 제대로 출력이 되는지 확인해보았다.

--- Folder05/anim_viewer.py
+++ Folder05/anim_viewer.py
@@ -2,11 +2,22 @@ from pico2d import *
 
 open_canvas()
 
+# 사진 크기가 577 * 500이므로 가로를 58(577 / 10) 세로를 62(500 / 8) 크기로 자르자
 character = load_image('The_Legend_of_Zelda_Link_Sprite_Sheet.png')
 
 
 def face_frame():
-    print('Face')
+    frame_y = 7
+    while frame_y > 3:
+        frame_x = 0
+        while frame_x < 3:
+            clear_canvas()
+            character.clip_draw(frame_x * 58, frame_y * 62, 58, 62, 400, 300)
+            update_canvas()
+            delay(1)
+            frame_x += 1
+
+        frame_y -= 1
 
 
 def front_frame():
@@ -27,12 +38,12 @@ def right_frame():
 
 face_frame()
 
-front_frame()
+#front_frame()
 
-left_frame()
+#left_frame()
 
-back_frame()
+#back_frame()
 
-right_frame()
+#right_frame()
 
 close_canvas()
