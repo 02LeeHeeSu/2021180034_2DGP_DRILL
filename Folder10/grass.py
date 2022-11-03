295014a008d8c11c54d4cabe58d6df165bf8bb28
Hee SU Lee <lee46911375@gmail.com>
2022-11-03 14:51:10 +0900

    Update grass.py
    Grass 객체의 깊이 정보를 알기 위해 depth 변수를 이용하여 초기화

--- Folder10/grass.py
+++ Folder10/grass.py
@@ -1,8 +1,9 @@
 from pico2d import *
 
 class Grass:
-    def __init__(self):
+    def __init__(self, depth=0):
         self.image = load_image('grass.png')
+        self.depth = depth
 
     def update(self):
         pass
